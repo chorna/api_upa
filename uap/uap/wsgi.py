7691edf5000ff2e38a55b086129399dec152e00d
Christian Horna <christian1020@gmail.com>
2018-12-04 11:25:01 -0500

    wsgi

--- uap/uap/wsgi.py
+++ uap/uap/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uap.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uap.uap.settings')
 
 application = get_wsgi_application()
