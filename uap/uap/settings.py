da1470ffdd9dd903407a7e016e9c1efb313b6f55
Christian Horna <christian1020@gmail.com>
2018-12-04 11:52:10 -0500

    settings

--- uap/uap/settings.py
+++ uap/uap/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api',
+    'uap.api',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'uap.urls'
+ROOT_URLCONF = 'uap.uap.urls'
 
 TEMPLATES = [
     {
